9fe94740478bcc7e0658e1e13d7f8db967e3b3d8
Ghadeer <ghadeer.ideasgate@gmail.com>
2019-10-03 14:36:34 +0200

    create php files

---
+++ php/body.blade.php
@@ -0,0 +1,209 @@
+ <!-- Slider section -->
+ <section id="mainSlider">
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+
+            </ol>
+            <div class="carousel-inner top">
+
+                <div class="carousel-item active">
+                    <img src="./images/slides/slide-1.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item top">
+                    <img src="./images/slides/slide-2.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item top">
+                    <img src="./images/slides/slide-3.jpg" class="d-block w-100" alt="..." />
+                </div>
+
+            </div>
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true">
+                </span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+    <!-- search section -->
+    <section id="search">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="d-flex justify-content-center">البحث فى المقالات </h4>
+                    <form class="form-inline my-3">
+                        <input class="form-control mr-sm-2 searchInput" type="search"
+                            placeholder="البحث فى مقالات الوقف" aria-label="Search" />
+                        <button class="searchBtn my-3 my-sm-0" type="submit">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- About section -->
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="mostviewedtitle col-md-12 my-5">
+                    <h3 class="sectionTitle d-flex justify-content-center"> من نحن </h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="vision">
+                        <h5>رؤيتنا</h5>
+                        <p>كل ما كتب عن الوقف بين يديك </p>
+                    </div>
+                    <div class="mission">
+                        <h5>مهمتنا
+                        </h5>
+                        <p> توفير رؤية عن الوقف من منظورات عدة من خلال المقالات المنشورة عن الوقف والأوقاف والسياسات
+                            الوقفية وتجارب العالم بها</p>
+
+                    </div>
+                    <div class="goals">
+                        <h5>اهدفنا</h5>
+                        <p>توفير كل ما هو مكتوب ومنشور عن الوقف
+
+                            إتاحة الفرصة للباحثين والمهتمين بالإلمام بالتجارب الوقفية
+
+                            تنقيح وتنسيق كل ما كتب بطريقة شيقة ومنظمة يسهل التعامل معاها </p>
+
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="aboutimgContainer">
+                        <div class="imgHolder">
+                            <img class="img-fluid" src="./images/about-us.jpg" />
+                        </div>
+                        <div class="imgborder"></div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+    <!-- Articles section -->
+    <section id="articles">
+        <div class="container">
+            <div class="row">
+                <div class="articles col-md-12">
+                    <h3 class="sectionTitle d-flex justify-content-center">مقالات الوقف</h3>
+                </div>
+            </div>
+            <div class="categoreis">
+                <div class="row mt-4">
+                    <div class="glyph col-md-3">
+
+                        <a href="category.html" target="_blank">
+                            <figure>
+                                <img src="./images/history.jpg" class="img-fluid" />
+                            </figure>
+                            <div class="class-name">
+                                <h5> تاريخ</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="glyph col-md-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="./images/researches.jpg" class="img-fluid">
+                            </figure>
+                            <div class="class-name">
+                                <h5>بحوث و دراسات</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="glyph col-md-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="./images/institutions.jpg" class="img-fluid" />
+                            </figure>
+                            <div class="class-name">
+                                <h5>مؤسسات</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="glyph col-md-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="./images/others.jpg" class="img-fluid" />
+                            </figure>
+                            <div class="class-name">
+                                <h5> أخرى</h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+    </section>
+    <!-- Most Viewed Section -->
+    <section id="most-viewed">
+        <div class="container">
+            <div class="row">
+                <div class="mostviewedtitle col-md-12 my-5">
+                    <h3 class="sectionTitle d-flex justify-content-center">الأكثر مشاهدة</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="grid">
+                        <figure class="effect-oscar">
+                            <img class="img-fluid" src="./images/pdf-covers/2.jpg" alt="صور مشرقه في القف الإسلامي ">
+                            <figcaption>
+                                <p class="icon-links">
+                                    <a href="#"><span class="icon-eye"><i class="fas fa-eye"></i></span> 200</a>
+                                    <a href="#"><span class="icon-paper-clip"><i
+                                                class="fas fa-book-reader"></i></span>قراءة/تحميل</a>
+                                </p>
+                                <p class="description">صور مشرقه في الوقف الإسلامي</p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="grid">
+                        <figure class="effect-oscar">
+                            <img class="img-fluid" src="./images/pdf-covers/3.jpg" alt="دور مؤتمرات العمل الخيري الخليجي في دعم
+وتطوير العمل الخيري الإسلامي ">
+                            <figcaption>
+                                <p class="icon-links">
+                                    <a href="#"><span class="icon-eye"><i class="fas fa-eye"></i></span> 200</a>
+                                    <a href="#"><span class="icon-paper-clip"><i
+                                                class="fas fa-book-reader"></i></span>قراءة/تحميل</a>
+                                </p>
+                                <p class="description">دور مؤتمرات العمل الخيري الخليجي في دعم
+                                    وتطوير العمل الخيري الإسلامي</p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="grid">
+                        <figure class="effect-oscar">
+                            <img class="img-fluid" src="./images/pdf-covers/5.jpg"
+                                alt="ثقافة الوقف بين الماضي والحاضر ">
+                            <figcaption>
+                                <p class="icon-links">
+                                    <a href="#"><span class="icon-eye"><i class="fas fa-eye"></i></span> 200</a>
+                                    <a href="#"><span class="icon-paper-clip"><i
+                                                class="fas fa-book-reader"></i></span>قراءة/تحميل</a>
+                                </p>
+                                <p class="description">ثقافة الوقف بين الماضي والحاضر </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
---
+++ php/footer.blade.php
@@ -0,0 +1,110 @@
+  <!-- footer section (contact us) -->
+  <footer id="contact">
+        <div class="backtoTop">
+
+            <a href="#top"  id="myBtn" title="الذهاب إلى أعلى"><i
+                    class="fas fa-angle-double-up"></i></a>
+
+        </div>
+        <div class="container">
+            <div class="row ustify-content-center">
+                <div class="col-md-12 text-center">
+                    <div class="footerlogo">
+                        <img src="images/footer-logo.png" alt="Logo" />
+                    </div>
+                    <div class="address">
+                        <p title="العنوان">4281 Express Lane Suite L7606,Sarasota,FL34238,USA</p>
+                    </div>
+                    <div class="email" title="البريد الإلكترونى"><a
+                            href="mailto:info@waqfonline.net">info@waqfonline.net</a></div>
+                    <div class="phone" title="الهاتف"><a href="tel:+443300010765"> <span
+                                class="plusSign">+</span>443300010765 </a>
+                    </div>
+                    <div class="social">
+
+                        <section>
+                            <ul id="footerSocial">
+                                <li>
+                                    <div class="facebook">
+                                        <a target="
+                                        -blank" href="http://www.facebook.com/waqfacademynet">
+                                            <i class="fab fa-facebook" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+
+                                </li>
+                                <li>
+                                    <div class="twitter">
+                                        <a target="
+                                        -blank" href="https://twitter.com/waqfacademy_">
+                                            <i class="fab fa-twitter" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="gplus">
+                                        <a target="
+                                        -blank" href="https://plus.google.com/u/0/113584762601619221710/posts">
+                                            <i class="fab fa-google-plus" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="youtube">
+                                        <a target="
+                                        -blank" href="https://www.youtube.com/user/waqfacademy">
+                                            <i class="fab fa-youtube" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+
+                                </li>
+                                <li>
+                                    <div class="linkedin">
+                                        <a target="
+                                        -blank"
+                                            href="http://www.linkedin.com/groups/%D8%A3%D9%83%D8%A7%D8%AF%D9%8A%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D9%88%D9%82%D9%81-5040142?goback=%2Eanp_5040142_1372737315341_1">
+                                            <i class="fab fa-linkedin" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+
+                                </li>
+                                <li>
+                                    <div class="instagram">
+                                        <a target="
+                                        -blank" href="http://instagram.com/waqfacademy">
+                                            <i class="fab fa-instagram" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="github">
+                                        <a target="
+                                        -blank" href="http://waqfacademy.blogspot.com/">
+                                            <i class="fab fa-blogger" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+
+                                </li>
+                            </ul>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <!-- script -->
+    <script src="./js/main.js"></script>
+</body>
+
+</html>
